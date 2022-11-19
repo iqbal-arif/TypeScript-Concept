0c4dc17cd5df80b97f351bf65bb76b1286bc6ca6
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2022-11-18 22:14:00 -0600

    Add files via upload

---
+++ NetNinja/Tuple/tup.ts
@@ -0,0 +1,15 @@
+// Normally Array or its items can be modified
+
+let arr = ["ruy",25,true];
+arr[0]= false;
+arr[1]="yougi";
+arr=[45,false,"sue"];
+
+// In TUPLES position type is predefined in an array. The value of each type can be changed, but not the type, such as string to boolean etc.
+let tup:[string,number,boolean] = ["jack",47,true]
+
+
+let student : [number,string]
+
+student =[45,"jack"]
+
